abe8b296fba3afed3cde1589af0f5e994b8f9145
Kevyn Santos <kevynsantos1804@gmail.com>
2026-01-19 16:04:15 -0800

    Add files via upload

---
+++ seminar_1/custom_shell_fix.c
@@ -0,0 +1,82 @@
+#include <sys/types.h> // allows us to use pid_t as a data type
+#include <sys/wait.h> //so we can use wait in the parent process
+#include <unistd.h> //allows us to use fork, exec, _exit
+#include <stdio.h> //basic input/output
+#include <string.h> // this is to be able to use strcomp, strlen, strcspn, etc
+
+int main()
+{
+    printf("Welcome to the lego shell. Type 'help' to see some commands\n");
+
+    char input[256]; // there are no strings so we have to define a big array of chars as our string
+
+    pid_t pid;
+
+    int newstate;
+
+    while (1)
+    {
+        printf("lego-shell> "); //shell thingy
+
+        fgets(input, sizeof(input), stdin); //used to read input. (where data will be stored, maximum size of string, what stream to read data from(stdin is default input stream))
+
+        input[strcspn(input, "\n")] = '\0'; //strcspn searches for a certain characters within a string and returns the index at where it begins.
+
+        if (strlen(input) == 0) //strlen returns the length of the "string". here we are checking for empty input
+        {
+            continue; //this makes it skip over to go back to beginning of loop
+        }
+        else if (strcmp(input, "exit") == 0) //strcmp compares two strings. 0 = identical, negative value if s1 is less than s2, positive value if s1 is greater than s2. negatives and positives is relevant because it checks the ascii value of the first char of each string
+        {
+            printf("exiting the legoverse...\n");
+            break; //this makes it exit the while loop and exit the shell as a whole
+        }
+        else if (strcmp(input, "help") == 0)
+        {
+            printf("\nLego Shell - Available Commands:\n\nhelp: Shows this help message\nexit: will exit the lego shell\ngato: try it :)\nMost other common shell commands available.\n\n");
+        }
+        else if (strcmp(input, "gato") == 0)
+        {
+            printf("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⠈⠻⣮⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡿⠋⠀⠀⠀⠀⠙⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠿⠿⣿⣷⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⡿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⠿⣿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⣀⣠⣤⣤⣀⡀⠀⠀⣀⣴⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣄⠀⠀\n⢀⣤⣾⡿⠟⠛⠛⢿⣿⣶⣾⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣷⣦⣀⣀⣤⣶⣿⡿⠿⢿⣿⡀⠀\n⣿⣿⠏⠀⢰⡆⠀⠀⠉⢿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⡿⠟⠋⠁⠀⠀⢸⣿⠇⠀\n⣿⡟⠀⣀⠈⣀⡀⠒⠃⠀⠙⣿⡆⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠇⠀\n⣿⡇⠀⠛⢠⡋⢙⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀\n⣿⣧⠀⠀⠀⠓⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠋⠀⠀⢸⣧⣤⣤⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡿⠀⠀\n⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠻⣷⣶⣶⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠁⠀⠀\n⠈⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⡏⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⡄⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⡄⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠿⠿⣿⣷⣶⣶⣤⣤⣀⡀⠀⠀⠀⢀⣴⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡿⣄\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠿⠿⣿⣷⣶⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣹\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⢸⣧\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣆⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣶⣾⣿⣿⣿⣿⣤⣄⣀⡀⠀⠀⠀⣿\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣻⣷⣶⣾⣿⣿⡿⢯⣛⣛⡋⠁⠀⠀⠉⠙⠛⠛⠿⣿⣿⡷⣶⣿\n");
+
+        }
+        else
+        {
+            
+            char *arguments[64]; //array of pointers for tokens to be stored to. ex. string: "hello world", arguments[0] = "hello", arguments[1] = "world"
+            int i = 0; //counter to keep track of where arguments are stored
+
+            char *currentArgument = strtok(input, " \t\n"); //strtok seperates a string into chunks called tokens. it is seperated based on the delimiter which in this case is a space, \t, or \n. it replaces these delimiters with \0 (null)
+
+            while (currentArgument != NULL) //loops while the current token is not null. basically it ends once there are no other arguments
+            {
+                arguments[i++] = currentArgument; //current token is stored into the array of pointers so that we can use them in execvp. increments after every while loop so that it stores properly
+                currentArgument = strtok(NULL, " \t\n"); //strtok remembers where it leaves off (the null character that replaces the delimiter with) which is why we use NULL instead of input. it still checks for delimiters.
+            }
+            arguments[i] = NULL; //once strtok is done with the string there is a null at the end which gets us out the loop but it doesnt save to end of the arguments array, which is why we add one here manually. this is needed by execvp, to know when the arguments have ended
+
+
+            pid = fork(); //fork creates a child process while the parent process continues running. 
+
+            if (pid < 0) //-1 = error occurred and child was not created. 
+                {
+                    printf("\n\nPROCESS API FAIL\n\n");
+                }
+            else if (pid == 0) //0 = child process occuring
+                {
+                    execvp(arguments[0], arguments); //we used execvp instead of execl because execl needs a list of the arguments. this means you need to know exactly how many arguments will be inputted. since this is not possible we used execvp. execvp uses a vector/array of arguments so it is more flexible for inputs that can be anything long. the p in execvP stands for path search. this means we dont need to put the exact path, it will just search for it
+                    perror("execvp failed"); //from here down it only happens if execvp does not run if it does run the process is replaced by the new process. perror prints error that occurs.
+                    _exit(127); //this terminates the process when command was not found/exec failed. this is in case someone types in random stuff and shell doesnt get stuck doing double processes.
+                }
+            else //else it is in parent process.
+                {
+                    waitpid(pid, &newstate, 0); //waits for child process to finish(which child process to wait for(0 means any child with the same process group id as calling process), here we store the exit status of child process,addition options for wait (in this casw 0 blocks the calling process until a specified child changes state) )
+                }
+
+        }
+
+    }
+
+    return 0;
+
+}
